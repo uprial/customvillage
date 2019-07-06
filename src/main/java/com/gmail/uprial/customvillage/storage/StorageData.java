dcbefe1002271f99e643f576c6c3bbdfb2269b97
Ilya Roslyakov <uprial@gmail.com>
2019-07-06 15:46:57 +0300

    Removed useless comment

--- src/main/java/com/gmail/uprial/customvillage/storage/StorageData.java
+++ src/main/java/com/gmail/uprial/customvillage/storage/StorageData.java
@@ -2,6 +2,5 @@ package com.gmail.uprial.customvillage.storage;
 
 import java.util.HashMap;
 
-//public class StorageData extends LinkedHashMap<String,String> {
 public class StorageData extends HashMap<String,String> {
 }
