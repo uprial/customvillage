95d7505880adbfabe90831f878c98f7a7fe2f8c3
Ilya Roslyakov <i_roslyakov@wargaming.net>
2024-09-27 00:26:55 +0300

    Aligned Formatter to other my plugins

--- src/main/java/com/gmail/uprial/customvillage/common/Formatter.java
+++ src/main/java/com/gmail/uprial/customvillage/common/Formatter.java
@@ -3,25 +3,28 @@ package com.gmail.uprial.customvillage.common;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.util.Vector;
 
 public final class Formatter {
     public static String format(Entity entity) {
-        if(entity == null) {
+        if (entity == null) {
             return "null";
         }
         Location location = entity.getLocation();
-        return String.format("%s[world: %s, x: %.0f, y: %.0f, z: %.0f]",
-                entity.getType().toString(),
+        return String.format("%s[w: %s, x: %.0f, y: %.0f, z: %.0f, hp: %.2f, id: %s]",
+                entity.getType(),
                 (location.getWorld() != null) ? location.getWorld().getName() : "empty",
-                location.getX(), location.getY(), location.getZ());
+                location.getX(), location.getY(), location.getZ(),
+                (entity instanceof LivingEntity) ? ((LivingEntity) entity).getHealth() : -1,
+                entity.getUniqueId());
     }
 
     public static String format(Vector vector) {
         if(vector == null) {
             return "null";
         }
-        return String.format("[x: %.2f, y: %.2f, z: %.2f, len: %.2f]",
+        return String.format("{x: %.2f, y: %.2f, z: %.2f, len: %.2f}",
                 vector.getX(), vector.getY(), vector.getZ(), vector.length());
     }
 
